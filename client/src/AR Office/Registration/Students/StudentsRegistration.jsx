50db5f4d20f7bd1d392731ecf3f21091f7fcb2df
SandunSenevirathna <sandunsandatharu@gmail.com>
2023-12-20 22:54:26 +0530

    make some small part in AddStudent.jsx

--- client/src/AR Office/Registration/Students/StudentsRegistration.jsx
+++ client/src/AR Office/Registration/Students/StudentsRegistration.jsx
@@ -1,155 +1,167 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { DataGrid } from "@mui/x-data-grid";
-import { Box } from "@mui/material";
-import RoundTextbox from "../../../Components/Textboxs/RoundTextbox";
+import { Box, Dialog, IconButton } from "@mui/material";
 import CustomButton from "../../../Components/Buttons/CutomButton/CustomButton";
+import AddStudent from "./Add Student/AddStudent";
+import axios from "axios";
+import config from "../../../ipAddress";
+import FastRewindRoundedIcon from "@mui/icons-material/FastRewindRounded";
 
 const StudentsRegistration = () => {
-  const [studentId, setStudentId] = useState("");
-  const [studentName, setStudentName] = useState("");
-  const [studentBatch, setStudentBatch] = useState("");
-  const [studentDepartment, setStudentDepartment] = useState("");
+  const localIp = config.localIp;
+  const [isAddStudentOpen, setIsAddStudentOpen] = useState(false);
+  const [studentAllData, setStudentAllData] = useState([]);
+  const [fetchDataTrigger, setFetchDataTrigger] = useState(true);
+  const [initialStudentData, setInitialStudentData] = useState({});
+  const [isNewStudent, setIsNewStudent] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          `http://${localIp}:8085/api/student/all`
+        );
+
+        const subjectsWithId = response.data.map((subject, index) => {
+          // Split the comma-separated values into arrays
+          const compulsoryDepartments = subject.compulsory_department
+            ? subject.compulsory_department.split(",")
+            : [];
+          const optionalDepartments = subject.optional_department
+            ? subject.optional_department.split(",")
+            : [];
+
+          // Check if arrays are empty or null and set "---" accordingly
+          const compulsoryDepartmentsToShow =
+            compulsoryDepartments.length > 0 ? compulsoryDepartments : ["---"];
+          const optionalDepartmentsToShow =
+            optionalDepartments.length > 0 ? optionalDepartments : ["---"];
+
+          return {
+            id: index + 1,
+            subject_code: subject.subject_code,
+            subject_name: subject.subject_name,
+            credit: subject.credit,
+            compulsory_departments: compulsoryDepartmentsToShow,
+            optional_departments: optionalDepartmentsToShow,
+            semester: subject.semester,
+          };
+        });
+
+        setStudentAllData(subjectsWithId);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    // Fetch data when the component mounts
+    fetchData();
+  }, [localIp, fetchDataTrigger]);
+
+  const handleOpenDialog = () => {
+    setIsAddStudentOpen(true);
+    setIsNewStudent(true);
+  };
+
+  const handleCloseDialog = () => {
+    setIsAddStudentOpen(false);
+    setFetchDataTrigger((prev) => !prev);
+  };
+  const handleOpenAddSubject = (rowData) => {
+    setIsAddStudentOpen(true);
+    setIsNewStudent(false);
+    setInitialStudentData(rowData);
+  };
 
   const columns = [
-    { field: "student_id", headerName: "S-ID", width: 200 },
-    { field: "student_name", headerName: "Student Name", width: 250 },
-    { field: "student_batch", headerName: "Batch", width: 70 },
-    { field: "student_department", headerName: "Department", width: 100 },
-    { field: "student_address", headerName: "Address", width: 200 },
-    { field: "student_tp", headerName: "TP Number", width: 100 },
-  ];
+    {
+      field: "student_registration_number",
+      headerName: "S-REG",
+      width: 150,
+    },
+    { field: "student_index_number", headerName: "S-Index", width: 100 },
+    { field: "student_name", headerName: "Student Name", width: 200 },
+    {
+      field: "batch",
+      headerName: "Batch",
+      width: 70,
+    },
+    {
+      field: "department",
+      headerName: "Departments",
+      width: 70,
+    },
+    { field: "address", headerName: "Address", width: 150 },
+    { field: "email", headerName: "Email", width: 150 },
+    { field: "tp_number", headerName: "TP", width: 100 },
 
-  const rows = [
+
     {
-      id:1,
-      student_id: "ITT-1819-079",
-      student_name: "W.A.M.S.S.Senevirathna",
-      student_batch: "18/19",
-      student_department: "ITT",
-      student_address: "Konaragama, Ehetuwewa, Galgamuwa",
-      student_tp: '0774941002',
-    },
-    {
-      id:2,
-      student_id: "ITT-1819-078",
-      student_name: "W.A.M.S.S.Senevirathna",
-      student_batch: "18/19",
-      student_department: "ITT",
-      student_address: "Konaragama, Ehetuwewa, Galgamuwa",
-      student_tp: '0774941002',
-    },
-    {
-      id:3,
-      student_id: "ITT-1819-072",
-      student_name: "W.A.M.S.S.Senevirathna",
-      student_batch: "18/19",
-      student_department: "ITT",
-      student_address: "Konaragama, Ehetuwewa, Galgamuwa",
-      student_tp: '0774941002',
-    },
-    {
-      id:4,
-      student_id: "ITT-1819-074",
-      student_name: "W.A.M.S.S.Senevirathna",
-      student_batch: "18/19",
-      student_department: "ITT",
-      student_address: "Konaragama, Ehetuwewa, Galgamuwa",
-      student_tp: '0774941002',
-    },
-    {
-      id:5,
-      student_id: "ITT-1819-073",
-      student_name: "W.A.M.S.S.Senevirathna",
-      student_batch: "18/19",
-      student_department: "ITT",
-      student_address: "Konaragama, Ehetuwewa, Galgamuwa",
-      student_tp: '0774941002',
+      field: "Actions_BillDelete",
+      headerName: "Select",
+      width: 80,
+      renderCell: (params) => (
+        <IconButton
+          color="warning"
+          onClick={() => handleOpenAddSubject(params.row)}
+        >
+          <FastRewindRoundedIcon />
+        </IconButton>
+      ),
     },
   ];
 
   return (
-    <div className={`flex flex-row space-x-32 ml-5 mr-5`}>
-      <Box
-        style={{ height: 500, width: "60%" }}
-        sx={{
-          "& .MuiDataGrid-columnHeaders": {
-            backgroundColor: "#333333",
-            borderBottom: "none",
-          },
-          "& .MuiDataGrid-columnHeaderTitle": {
-            color: "white",
+    <div className={`flex flex-col ${isAddStudentOpen ? "blur-md" : ""} `}>
+      <Box display="flex" justifyContent="flex-end">
+        <CustomButton
+          onClick={handleOpenDialog}
+          title="Add Student"
+          backgroundColor="#333333"
+          borderRadius={10}
+          width={130}
+          height={40}
+        />
+      </Box>
+
+      <div className="mt-2 items-center justify-center h-screen">
+        <Box
+          style={{ height: "80vh", width: "auto" }}
+          sx={{
+            "& .MuiDataGrid-columnHeaders": {
+              backgroundColor: "#333333",
+              borderBottom: "none",
+            },
+            "& .MuiDataGrid-columnHeaderTitle": {
+              color: "white",
+            },
+          }}
+        >
+          <DataGrid
+            disableRowSelectionOnClick
+            rows={studentAllData}
+            columns={columns}
+            autoPageSize
+          />
+        </Box>
+      </div>
+
+      <Dialog
+        open={isAddStudentOpen}
+        onClose={handleCloseDialog}
+        PaperProps={{
+          style: {
+            borderRadius: "15px",
+            // Add other custom styles as needed
           },
         }}
       >
-        <DataGrid rows={rows} columns={columns} />
-      </Box>
-
-      <Box
-        display="flex"
-        flexDirection="column"
-        alignItems="center"
-        justifyContent="center"
-      >
-        <div className="flex flex-col space-y-5">
-          <RoundTextbox
-            className={"text-center"}
-            value={studentId || ""}
-            onChange={() => {}}
-            height="40px"
-            placeholder="Student ID"
-          />
-          <RoundTextbox
-            className={"text-center"}
-            value={studentName || ""}
-            onChange={() => {}}
-            height="40px"
-            placeholder="Student Name"
-          />
-          <RoundTextbox
-            className={"text-center"}
-            value={studentBatch || ""}
-            onChange={() => {}}
-            height="40px"
-            placeholder="Batch"
-          />
-          <RoundTextbox
-            className={"text-center"}
-            value={studentDepartment || ""}
-            onChange={() => {}}
-            height="40px"
-            placeholder="Department"
-          />
-        </div>
-
-        <div className="flex flex-row mt-5 space-x-3 ">
-          <CustomButton
-            onClick={() => {}}
-            title="Add"
-            backgroundColor="#333333"
-            borderRadius={30}
-            width="120px"
-            height="40px"
-          />
-          <CustomButton
-            onClick={() => {}}
-            title="Delete"
-            backgroundColor="#333333"
-            borderRadius={30}
-            width="100px"
-            height="40px"
-          />
-        </div>
-        <div className="mt-3">
-          <CustomButton
-            onClick={() => {}}
-            title="clean"
-            backgroundColor="#333333"
-            borderRadius={30}
-            width="230px"
-            height="40px"
-          />
-        </div>
-      </Box>
+        <AddStudent
+          onClose={handleCloseDialog}
+          initialSubjectData={initialStudentData}
+          isNewSubject={isNewStudent}
+        />
+      </Dialog>
     </div>
   );
 };
